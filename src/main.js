e2341ba86f044ae63f288fe4410decfa55853765
liyu <lllyyy2012@icloud.com>
2017-04-04 11:56:45 +0800

    第一次学习项目， 代码是组件嵌套关系相关的代码

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+let layout = {
+    template: '<div>我是内部挂载的组件<global-component></global-component><child></child></div>',
+    components: {
+        'child': {
+            template: '<span>我是一个普通组件内部的组件</span>'
+        }
+    }
+}
+Vue.component('global-component', {
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
+    template: '<h2>I\'m a global component </h2>'
+})
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+    el: "#app",
+    components: {
+        "layout": layout
+    }
+
 })
